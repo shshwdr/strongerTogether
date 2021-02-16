e4a6c513ed98ef5a94ea1b302cc77ed5b38cee28
shshwdr <cs.vivian04@gmail.com>
2021-02-15 22:46:30 -0600

    add attack for skeleton

---
+++ Assets/EnemyBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public bool ignoreOtherCollider = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<PlayerController>())
+        {
+            collision.collider.GetComponent<PlayerController>().getDamage();
+        }
+        else if (ignoreOtherCollider)
+        {
+            return;
+        }
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>())
+        {
+            collision.GetComponent<PlayerController>().getDamage();
+        }
+    }
+}
---
+++ Assets/EnemyRangeAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangeAttack : MonoBehaviour
+{
+    
+    public GameObject bulletPrefab;
+    public float cooldownTime = 2f;
+    public float bulletSpeed = 10f;
+    float currentCooldownTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Attack()
+    {
+        var player = EnemyManager.instance.player;
+        var dir = player.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        var quat = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, quat) as GameObject;
+        //bullet.GetComponent<EnemyBullet>().GetPlayer(player.transform);
+        bullet.GetComponent<Rigidbody2D>().velocity = (player.transform.position - transform.position).normalized * bulletSpeed;
+        //bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        //bullet.GetComponent<EnemyBullet>().isEnemyBullet = true;
+        //StartCoroutine(CoolDown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCooldownTimer += Time.deltaTime;
+        if (currentCooldownTimer > cooldownTime)
+        {
+            currentCooldownTimer = 0;
+            Attack();
+        }
+    }
+}
